5c4c35f598273f0b146fd831ab7b3f03339fe23d
Kesler Mathieu <kmat5@icloud.com>
2023-03-21 21:22:20 -0400

    confirm movement on one motor

--- test_motors.py
+++ test_motors.py
@@ -17,28 +17,24 @@ MOTOR_A_SPEED_ENABLE = 15
 MOTOR_B_SPEED_ENABLE = 22
 
 # Uses pin numbers not GPIO numbers. Refer to Wiring diagram in above url which illustrates pin numbering being used
-def setup() -> None:
-    GPIO.setmode(GPIO.BOARD)
-    # setup all pins as output
-    GPIO.setup(INPUT_PIN_1_MOTOR_A, GPIO.OUT)
-    GPIO.setup(INPUT_PIN_2_MOTOR_A, GPIO.OUT)
+print("setting up board...")
+GPIO.setmode(GPIO.BOARD)
+# setup all pins as output
+GPIO.setup(INPUT_PIN_1_MOTOR_A, GPIO.OUT)
+GPIO.setup(INPUT_PIN_2_MOTOR_A, GPIO.OUT)
 
-    GPIO.setup(INPUT_PIN_3_MOTOR_B, GPIO.OUT)
-    GPIO.setup(INPUT_PIN_4_MOTOR_B, GPIO.OUT)
-    GPIO.setup(MOTOR_A_SPEED_ENABLE, GPIO.OUT)
-    GPIO.setup(MOTOR_B_SPEED_ENABLE, GPIO.OUT)
-    p1 = GPIO.PWM(MOTOR_A_SPEED_ENABLE, 100)
-    p2 = GPIO.PWM(MOTOR_B_SPEED_ENABLE, 100)
-    p1.start(50) # 50% duty cycle or 50% power
-    p2.start(50)
+GPIO.setup(INPUT_PIN_3_MOTOR_B, GPIO.OUT)
+GPIO.setup(INPUT_PIN_4_MOTOR_B, GPIO.OUT)
+GPIO.setup(MOTOR_A_SPEED_ENABLE, GPIO.OUT)
+GPIO.setup(MOTOR_B_SPEED_ENABLE, GPIO.OUT)
+
+# Reference on Pulse Width Modulation (PWM): https://raspi.tv/2013/rpi-gpio-0-5-2a-now-has-software-pwm-how-to-use-it
+p1 = GPIO.PWM(MOTOR_A_SPEED_ENABLE, 100)
+#p2 = GPIO.PWM(MOTOR_B_SPEED_ENABLE, 100)
+p1.start(50) # 50% duty cycle or 50% power
+#p2.start(50)
 
 # Refer to table here for HIGH/LOW combinations <-> movement: https://lastminuteengineers.com/l298n-dc-stepper-driver-arduino-tutorial/
-def motor_off() -> None:
-    GPIO.output(INPUT_PIN_1_MOTOR_A, GPIO.LOW)
-    GPIO.output(INPUT_PIN_2_MOTOR_A, GPIO.LOW)
-    GPIO.output(INPUT_PIN_3_MOTOR_B, GPIO.LOW)
-    GPIO.output(INPUT_PIN_4_MOTOR_B, GPIO.LOW)
-
 def move_rover_forward() -> None:
     GPIO.output(INPUT_PIN_1_MOTOR_A, GPIO.HIGH)
     GPIO.output(INPUT_PIN_2_MOTOR_A, GPIO.LOW)
@@ -55,8 +51,6 @@ def move_rover_backward() -> None:
 
 # Good video tutorial: https://www.youtube.com/watch?v=Qp4wNdyC2Z0
 def main():
-    print("setting up board...")
-    setup()
     try:
         while True:
             move_rover_forward()
@@ -69,7 +63,7 @@ def main():
     finally:
         print("stopping pwm on both motors...")
         p1.stop()
-        p2.stop()
+        #p2.stop()
         print("gpio cleanup...")
         GPIO.cleanup()
     
