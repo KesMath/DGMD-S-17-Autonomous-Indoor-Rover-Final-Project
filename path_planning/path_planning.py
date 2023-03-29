4b8349db56353c8a2c75783663cad9894958da02
Kesler Mathieu <kmat5@icloud.com>
2023-03-29 17:15:29 -0400

    adding node class

--- path_planning/path_planning.py
+++ path_planning/path_planning.py
@@ -1,8 +1,24 @@
+class Node:
+    def __init__(self, grid_cost: int, parent) -> None:
+        self.grid_cost = grid_cost
+        self.parent = parent
+    
+    def update_grid_cost(self, val: int) -> None:
+        self.grid_cost = val
+    
+    def get_grid_cost(self) -> int:
+        return self.grid_cost
+
+    def get_parent(self):
+        return self.parent
+    
+
+
 def dijkstra(start_index, goal_index, width, height, costmap, resolution, origin):
   ''' 
   Performs Dijkstra's shortest path algorithm search on a costmap with a given start and goal node
   '''
-    
+
     
   pass
 
@@ -10,6 +26,5 @@ def dijkstra(start_index, goal_index, width, height, costmap, resolution, origin
 
 def main():
     pass
-
 if __name__ == '__main__':
     main()
