fd9ab5e77078065982640174723d495619ef0d82
Kesler Mathieu <kmat5@icloud.com>
2023-03-29 16:56:51 -0400

    adding function template

---
+++ path_planning/path_planning.py
@@ -0,0 +1,15 @@
+def dijkstra(start_index, goal_index, width, height, costmap, resolution, origin):
+  ''' 
+  Performs Dijkstra's shortest path algorithm search on a costmap with a given start and goal node
+  '''
+    
+    
+  pass
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
