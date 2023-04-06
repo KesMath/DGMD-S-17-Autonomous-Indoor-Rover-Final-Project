d8342abd7350c9438e59594e8c29cc8953dd5e57
Kesler Mathieu <kmat5@icloud.com>
2023-04-06 14:42:32 -0400

    initial commit

---
+++ lidar/scan1.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+from math import floor
+from adafruit_rplidar import RPLidar
+
+# Setup the RPLidar
+PORT_NAME = "/dev/ttyUSB0"
+lidar = RPLidar(None, PORT_NAME, timeout=3)
+
+lidar.connect()
+
+try:
+    for scan in lidar.iter_scans():
+        print('********************************************')
+        print('********************************************')
+        
+        for (_, angle, distance) in scan:
+            #scan_data[min([359, floor(angle)])] = distance
+            print(angle)
+            print(distance)
+            print('*****')
+        #process_data(scan_data)
+        
+except KeyboardInterrupt:
+    print("Stopping.")
+    lidar.stop()
+    lidar.stop_motor()
+    
+#lidar.disconnect()
+
