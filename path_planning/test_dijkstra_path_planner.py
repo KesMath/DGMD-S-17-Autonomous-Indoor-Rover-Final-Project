1a1b63ef9d1b1d270951cb37b2740afe79baddc2
Kesler Mathieu <kmat5@icloud.com>
2023-04-15 14:48:45 -0400

    completing 1st test

--- path_planning/test_dijkstra_path_planner.py
+++ path_planning/test_dijkstra_path_planner.py
@@ -14,14 +14,24 @@ class TestPathPlanner(unittest.TestCase):
         expected = list()
         parent_node = Node(grid_cost = 0, coordinate_pt = (2,2), parent = None)
 
-        expected.append(Node(grid_cost = 1, coordinate_pt = (1,2), parent = parent_node)) # UP
         expected.append(Node(grid_cost = 1, coordinate_pt = (2,1), parent = parent_node)) # LEFT
         expected.append(Node(grid_cost = 1, coordinate_pt = (2,3), parent = parent_node)) # RIGHT
+        expected.append(Node(grid_cost = 1, coordinate_pt = (1,2), parent = parent_node)) # UP
         expected.append(Node(grid_cost = 1, coordinate_pt = (3,2), parent = parent_node)) # DOWN
 
         actual = find_neighbours(node=parent_node, width=5, height=5, gridmap=OCCUPANCY_GRID, resolution=STEP_COST)
-        for node in actual:
-            print(node)
+        
+        # assert coordinates are as expected
+        self.assertEqual(expected[0].get_coordinate_pt(), actual[0].get_coordinate_pt())
+        self.assertEqual(expected[1].get_coordinate_pt(), actual[1].get_coordinate_pt())
+        self.assertEqual(expected[2].get_coordinate_pt(), actual[2].get_coordinate_pt())
+        self.assertEqual(expected[3].get_coordinate_pt(), actual[3].get_coordinate_pt())
+        
+        # assert grid cost is as expected
+        self.assertEqual(expected[0].grid_cost, actual[0].grid_cost)
+        self.assertEqual(expected[1].grid_cost, actual[1].grid_cost)
+        self.assertEqual(expected[2].grid_cost, actual[2].grid_cost)
+        self.assertEqual(expected[3].grid_cost, actual[3].grid_cost)
         
 
     def test_bottom_left_cell_returns_two_neighbors(self):
