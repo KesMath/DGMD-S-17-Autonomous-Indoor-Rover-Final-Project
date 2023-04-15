7c017267448a605252b6e0ac904fb20fff0532e5
Kesler Mathieu <kmat5@icloud.com>
2023-04-15 15:51:04 -0400

    adding test_cell_ignores_two_occupied_neighbors()

--- path_planning/test_dijkstra_path_planner.py
+++ path_planning/test_dijkstra_path_planner.py
@@ -166,6 +166,35 @@ class TestPathPlanner(unittest.TestCase):
             self.assertEqual(node.parent, parent_node)
 
 
+    def test_cell_ignores_two_occupied_neighbors(self):
+        expected = list()
+        parent_node = Node(grid_cost = 0, coordinate_pt = (2,2), parent = None)
+        temp_grid = deepcopy(OCCUPANCY_GRID)
+
+        # occupying left and righht adjacent neighbors
+        temp_grid[2][1] = 1  # LEFT
+        temp_grid[2][3] = 1  # RIGHT
+
+        actual = find_neighbours(node=parent_node, width=5, height=5, gridmap=temp_grid, resolution=STEP_COST)
+
+        expected.append(Node(grid_cost = 1, coordinate_pt = (1,2), parent = parent_node)) # UP
+        expected.append(Node(grid_cost = 1, coordinate_pt = (3,2), parent = parent_node)) # DOWN
+
+        assert(len(actual) == len(expected))
+
+        # assert coordinates are as expected
+        self.assertEqual(expected[0].get_coordinate_pt(), actual[0].get_coordinate_pt())
+        self.assertEqual(expected[1].get_coordinate_pt(), actual[1].get_coordinate_pt())
+        
+        # assert grid cost is as expected
+        self.assertEqual(expected[0].grid_cost, actual[0].grid_cost)
+        self.assertEqual(expected[1].grid_cost, actual[1].grid_cost)
+
+        # assert parent node is as expected
+        for node in actual:
+            self.assertEqual(node.parent, parent_node)
+
+
     
     def test_cell_has_no_free_neighbors(self):
         parent_node = Node(grid_cost = 0, coordinate_pt = (2,2), parent = None)
