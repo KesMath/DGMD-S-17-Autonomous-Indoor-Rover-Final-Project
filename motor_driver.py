4886ae0b451e1833ed7dc1f9e7720e6d179afbbc
Kesler Mathieu <kmat5@icloud.com>
2023-04-22 16:20:28 -0400

    fleshing out class

--- motor_driver.py
+++ motor_driver.py
@@ -1,22 +1,26 @@
 import time
 import RPi.GPIO as GPIO
+from typing import Final
 
 # Refer to L298 Motor Driver Documentation: https://www.electroduino.com/introduction-to-l298n-motor-driver-how-its-work/
 
 # Pins for Direction Control
 # Refer to https://docs.viam.com/try-viam/rover-resources/rover-tutorial/ to determine Raspberry Pi Pin Mapping! 
-INPUT_PIN_1_MOTOR_A = 11
-INPUT_PIN_2_MOTOR_A = 13
+INPUT_PIN_1_MOTOR_A: Final[int] = 11
+INPUT_PIN_2_MOTOR_A: Final[int] = 13
 
-INPUT_PIN_3_MOTOR_B = 16
-INPUT_PIN_4_MOTOR_B = 18
+INPUT_PIN_3_MOTOR_B: Final[int] = 16
+INPUT_PIN_4_MOTOR_B: Final[int] = 18
 
 # Pins for Speed Control
-MOTOR_A_SPEED_ENABLE = 15
-MOTOR_B_SPEED_ENABLE = 22
+MOTOR_A_SPEED_ENABLE: Final[int] = 15
+MOTOR_B_SPEED_ENABLE: Final[int] = 22
 
-FREQUENCY = 100 # is the number of times per second that a pulse is generated.
-DUTY_CYLCE = 50 # is the percentage of time between pulses that the signal is “high” or “On”.
+FREQUENCY: Final[int] = 100 # is the number of times per second that a pulse is generated.
+DUTY_CYLCE: Final[int] = 50 # is the percentage of time between pulses that the signal is “high” or “On”.
+
+####### WARNING!! - VALUE HERE IS DEPENDENT ON FREQ=100/DUTY_CYCLE=50 #######
+MILLESECOND_DELAY_FOR_1_FOOT_TRAVEL: Final[float] = 0.00125 # experimentally timed milleseconds needed for rover to travel 1 foot
 
 class MotorDriver:
     def __init__(self):
@@ -46,40 +50,67 @@ class MotorDriver:
         return self.p2
         
     # Refer to table here for HIGH/LOW combinations <-> movement: https://lastminuteengineers.com/l298n-dc-stepper-driver-arduino-tutorial/
-    def move_rover_forward(self) -> None:
+    def __move_rover_forward(self) -> None:
         GPIO.output(INPUT_PIN_1_MOTOR_A, GPIO.HIGH)
         GPIO.output(INPUT_PIN_2_MOTOR_A, GPIO.LOW)
 
         GPIO.output(INPUT_PIN_3_MOTOR_B, GPIO.HIGH)
         GPIO.output(INPUT_PIN_4_MOTOR_B, GPIO.LOW)
 
-    def move_rover_backward(self) -> None:
+    def __move_rover_backward(self) -> None:
         GPIO.output(INPUT_PIN_1_MOTOR_A, GPIO.LOW)
         GPIO.output(INPUT_PIN_2_MOTOR_A, GPIO.HIGH)
 
         GPIO.output(INPUT_PIN_3_MOTOR_B, GPIO.LOW)
         GPIO.output(INPUT_PIN_4_MOTOR_B, GPIO.HIGH)
 
+    
+    def pivot_left_90_deg(self) -> None:
+        pass
+
+    def pivot_right_90_deg(self) -> None:
+        pass
+
+    def move_forward_1_foot(self) -> None:
+        while True:
+            __move_rover_forward()
+            time.sleep(MILLESECOND_DELAY_FOR_1_FOOT_TRAVEL * 1000)
+            break
+
+        self.p1.stop() # stop PWM output
+        self.p2.stop() # stop PWM output
+
+
+    def move_backward_1_foot(self) -> None:
+        pass
+
+    def move_right_1_foot(self) -> None:
+        pass
+    
+    def move_left_1_foot(self) -> None:
+        pass
 
 
 def main():
-    m = MotorDriver()
-    try:
-        while True:
-            m.move_rover_forward()
-            time.sleep(10)
-            m.move_rover_backward()
-            time.sleep(10)
+    motor = MotorDriver()
+    motor.move_forward_1_foot()
+    GPIO.cleanup()
+    # try:
+    #     while True:
+    #         m.move_rover_forward()
+    #         time.sleep(10)
+    #         m.move_rover_backward()
+    #         time.sleep(10)
             
-    except KeyboardInterrupt:
-        print("keyboard interupt detected...")
-        pass
-    finally:
-        print("stopping pwm on both motors...")
-        m.get_motor1().stop()
-        m.get_motor2().stop()
-        print("gpio cleanup...")
-        GPIO.cleanup()
+    # except KeyboardInterrupt:
+    #     print("keyboard interupt detected...")
+    #     pass
+    # finally:
+    #     print("stopping pwm on both motors...")
+    #     m.get_motor1().stop()
+    #     m.get_motor2().stop()
+    #     print("gpio cleanup...")
+    #     GPIO.cleanup()
     
 if __name__ == '__main__':
     main()
