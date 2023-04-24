056c9e2263f3ce70c0624f89032fb9f5ad9d2ac1
Kesler Mathieu <kmat5@icloud.com>
2023-04-24 18:49:59 -0400

    adding drive to next tile logic

--- motor_driver.py
+++ motor_driver.py
@@ -3,7 +3,7 @@ import asyncio
 from viam.robot.client import RobotClient
 from viam.rpc.dial import Credentials, DialOptions
 from viam.components.base import Base
-
+from path_planning import * 
 
 async def connect():
     creds = Credentials(
@@ -48,13 +48,39 @@ async def drive_left_1_foot(base):
     # re-centers rover forward
     await spin_right_90_degrees(base)
 
+async def drive_to_next_tile(base, current_point: tuple, new_coordinate_pt: tuple):
+    # drive to left tile
+    if new_coordinate_pt[0] < current_point[0]:
+        await drive_left_1_foot(base)
+
+    # drive to right tile
+    elif new_coordinate_pt[1] > current_point[1]:
+        await drive_right_1_foot(base)
+    
+    # drive forward
+    elif new_coordinate_pt[0] < current_point[0]:
+        await move_forward_1_foot(base)
+    
+    # TODO: add remaining cases
+    # drive backward
+
+    # stay put!
+
 async def main():
     robot = await connect()
-    
+    start_point = (4,0)
     # Get the base component from the Viam Rover
     roverBase = Base.from_robot(robot, 'viam_base')
+    shortest_path = return_shortest_path(start_point = start_point, goal_point = (0,4), width = GRID_WIDTH, height = GRID_HEIGHT, gridmap= EMPTY_GRID, resolution = STEP_COST)
 
-    await drive_left_1_foot(roverBase)
+    for node in shortest_path:
+        next_point = node.get_coordinate_pt()
+        print("driving to :" + next_point)
+        await drive_to_next_tile(base = robot, current_point = start_point, new_coordinate_pt = next_point)
+        time.sleep(2)
+        # need to update starting point since robot moved to a new position
+        start_point = next_point
+    
 
     # close server connection
     await robot.close()
