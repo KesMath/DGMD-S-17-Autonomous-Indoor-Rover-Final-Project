ca3b2e4f62c5cf87450eefd9b7fd75a6ade12867
Kesler Mathieu <kmat5@icloud.com>
2023-04-24 12:48:09 -0400

    remove test function

--- motor_driver.py
+++ motor_driver.py
@@ -15,15 +15,6 @@ async def connect():
     )
     return await RobotClient.at_address('kes-rover-main.0ltqp6fjer.viam.cloud', opts)
 
-async def moveInSquare(base):
-    for _ in range(4):
-        # moves the Viam Rover forward 500mm at 500mm/s
-        await base.move_straight(velocity=500, distance=500)
-        print("move straight")
-        # spins the Viam Rover 90 degrees at 100 degrees per second
-        await base.spin(velocity=100, angle=90)
-        print("spin 90 degrees")
-
 async def move_forward_1_foot(base):
     # moves the Viam Rover forward 625mm at 625mm/s
     await base.move_straight(velocity=625, distance=625)
@@ -48,9 +39,6 @@ async def main():
     
     # Get the base component from the Viam Rover
     roverBase = Base.from_robot(robot, 'viam_base')
-
-    # Move the Viam Rover in a square
-    #await moveInSquare(roverBase)
 
     await move_forward_1_foot(roverBase)
 
