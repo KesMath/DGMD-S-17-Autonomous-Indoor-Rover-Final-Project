b3cef60d7450d14a88a224c1de930663e804920c
Kesler Mathieu <kmat5@icloud.com>
2023-04-24 12:42:19 -0400

    rover driving 1 foot successfully

--- motor_driver.py
+++ motor_driver.py
@@ -25,8 +25,8 @@ async def moveInSquare(base):
         print("spin 90 degrees")
 
 async def move_forward_1_foot(base):
-    # moves the Viam Rover forward 1000mm at 1000mm/s
-    await base.move_straight(velocity=1000, distance=1000)
+    # moves the Viam Rover forward 625mm at 625mm/s
+    await base.move_straight(velocity=625, distance=625)
     print("move straight")
 
 
