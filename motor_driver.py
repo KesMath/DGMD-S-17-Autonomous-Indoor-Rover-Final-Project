52c005dc4f48671be99fff2d9b566b0de4ecc597
Kesler Mathieu <kmat5@icloud.com>
2023-04-24 20:25:04 -0400

    ducktaping import statements

--- motor_driver.py
+++ motor_driver.py
@@ -4,9 +4,8 @@ from viam.robot.client import RobotClient
 from viam.rpc.dial import Credentials, DialOptions
 from viam.components.base import Base
 
-from path_planning import grid_maps
-from path_planning import node
-from path_planning import dijkstra_path_planner
+from path_planning.dijkstra_path_planner import *
+from path_planning.grid_maps import *
 
 async def connect():
     creds = Credentials(
