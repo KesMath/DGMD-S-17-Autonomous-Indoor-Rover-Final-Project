0d89eba1e63b1cddbf3f7c49e2b30b474dfcf568
Kesler Mathieu <kmat5@icloud.com>
2023-05-01 17:54:15 -0400

    modifying program

--- lidar/scan_enclosure.py
+++ lidar/scan_enclosure.py
@@ -10,24 +10,25 @@ def scan_enclosure() -> pd.DataFrame:
     """
     # setup connection to device
     SAMPLING_TIME = 25
-    
+    PORT_NAME = "/dev/ttyUSB0"
+    lidar = RPLidar(None, PORT_NAME, timeout=3)
+    lidar.connect()
+
     sensor_data = list()
     # iterating for 20 sec and persisting lidar reading to 2D array
     t_end = time.time() + SAMPLING_TIME
     while time.time() < t_end:
-        for scan in lidar.iter_scans():       
+        for scan in lidar.iter_scans():
+            print("daving lidar data to dataframe")       
             for (_, angle, distance) in scan:
-                print(angle)
-                print(distance)
+                #print(angle)
+                #print(distance)
                 sensor_data.append([angle, distance])
 
     # disconnecting resource
+    lidar.stop()
+    lidar.stop_motor()
     return pd.DataFrame(sensor_data)
 
 if __name__ == '__main__':
-    PORT_NAME = "/dev/ttyUSB0"
-    lidar = RPLidar(None, PORT_NAME, timeout=3)
-    lidar.connect()
     scan_enclosure()
-    lidar.stop()
-    lidar.stop_motor()
