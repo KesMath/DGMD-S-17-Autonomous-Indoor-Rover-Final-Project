c8e9be274dd4a56914022ae45665fd1114adc981
Kesler Mathieu <kmat5@icloud.com>
2023-05-03 10:45:26 -0400

    small modification

--- lidar/scan1.py
+++ lidar/scan1.py
@@ -12,19 +12,14 @@ lidar = RPLidar(None, PORT_NAME, timeout=3)
 lidar.connect()
 
 try:
-    for scan in lidar.iter_scans():
-        print('********************************************')
-        print('********************************************')
-        
+    for scan in lidar.iter_scans():        
         for (_, angle, distance) in scan:
             #scan_data[min([359, floor(angle)])] = distance
-            print(angle)
-            print(distance)
-            print('*****')
+            print(angle, distance)
         #process_data(scan_data)
         
 except KeyboardInterrupt:
-    print("Stopping.")
+    #print("Stopping.")
     lidar.stop()
     lidar.stop_motor()
     
