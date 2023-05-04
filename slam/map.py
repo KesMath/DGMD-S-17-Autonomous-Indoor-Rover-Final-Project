d3ebe79f0df158dfbea74642e502f19443b707b2
Kesler Mathieu <kmat5@icloud.com>
2023-05-03 20:26:46 -0400

    saving mapping changes

--- slam/map.py
+++ slam/map.py
@@ -7,15 +7,16 @@ Created on Mon Apr 24 10:29:13 2023
 
 import pandas
 import matplotlib.pyplot as plt
-import math    
+import math
+import sys    
 
-df = pandas.read_csv('slam/sampling_profile_1.csv', names=['angle', 'distance','q'])
+df = pandas.read_csv('slam/sampling.csv', names=['angle', 'distance','q'])
 
 plt.cla()
-plt.ylim(-2000,3000)
-plt.xlim(-4000,2000)
+plt.ylim(-850,850)
+plt.xlim(-850,850)
 
-for row  in df.iterrows():
+for row in df.iterrows():
   angle, distance, q = row[1]
   
   x = distance * math.cos(math.radians(float(angle)))     
@@ -23,5 +24,6 @@ for row  in df.iterrows():
 
   plt.plot(x,y,marker='.',color='red')
 
+print("saving image ...")
 plt.savefig('lidar_map.png', bbox_inches='tight')
-plt.show()
+sys.exit(0)
