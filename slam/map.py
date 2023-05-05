a31301e0202e7fa030f92928c55fff9618280b94
Kesler Mathieu <kmat5@icloud.com>
2023-05-04 20:55:15 -0400

    restoring file to HEAD

--- slam/map.py
+++ slam/map.py
@@ -1,29 +1,26 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr 24 10:29:13 2023
-
 @author: HUNGTRAN
 """
 
 import pandas
 import matplotlib.pyplot as plt
-import math
-import sys    
+import math    
 
-df = pandas.read_csv('slam/sampling.csv', names=['angle', 'distance','q'])
+df = pandas.read_csv('lidar_data_01.csv', names=['angle', 'distance','q'])
 
 plt.cla()
-plt.ylim(-850,850)
-plt.xlim(-850,850)
+plt.ylim(-2000,3000)
+plt.xlim(-4000,2000)
 
-for row in df.iterrows():
+for row  in df.iterrows():
   angle, distance, q = row[1]
-  
-  x = distance * math.cos(math.radians(float(angle)))     
-  y = distance * math.sin(math.radians(float(angle)))
+
+  x = distance * math.cos(math.radians(angle))     
+  y = distance * math.sin(math.radians(angle))
 
   plt.plot(x,y,marker='.',color='red')
 
-print("saving image ...")
 plt.savefig('lidar_map.png', bbox_inches='tight')
-sys.exit(0)
+plt.show()
