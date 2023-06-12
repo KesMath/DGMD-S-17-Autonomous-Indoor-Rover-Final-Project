b3558435cb1f7cacc1081c6508aeba832fb8d57e
Kesler Mathieu <kmat5@icloud.com>
2023-06-12 19:33:34 -0400

    test code change

--- video/video_capture.py
+++ video/video_capture.py
@@ -1,67 +1,40 @@
-# import cv2 as cv
+import cv2 as cv
 
-# class VideoCapture():
-#     def __init__(self):
-#         self.cap = None
-#         self.outfile = None
+class VideoCapture():
+    def __init__(self):
+        self.cap = None
+        self.outfile = None
 
-#     def capture_video(self):
-#         self.cap = cv.VideoCapture(4) # corresponds to /dev/video4 character device file
+    def capture_video(self):
+        self.cap = cv.VideoCapture(4) # corresponds to /dev/video4 character device file
 
-#         # Define the codec and create VideoWriter object
-#         fourcc = cv.VideoWriter_fourcc(*'XVID')
+        # Define the codec and create VideoWriter object
+        fourcc = cv.VideoWriter_fourcc(*'XVID')
 
-#         self.outfile = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
+        self.outfile = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
 
-#         while self.cap.isOpened():
-#             ret, frame = self.cap.read()
-#             if not ret:
-#                 print("Can't receive frame (stream end?). Exiting ...")
-#                 break
-#             frame = cv.flip(frame, 0)
-#             # write the flipped frame
-#             self.outfile.write(frame)
-#             cv.imshow('frame', frame)
-#             if cv.waitKey(1) == ord('q'):
-#                 break
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+            frame = cv.flip(frame, 0)
+            # write the flipped frame
+            self.outfile.write(frame)
+            cv.imshow('frame', frame)
+            if cv.waitKey(1) == ord('q'):
+                break
 
-#     def free_video_resources(self):      
-#         # Release everything if job is finished
-#         self.cap.release()
-#         self.outfile.release()
-#         cv.destroyAllWindows()
+    def free_video_resources(self):      
+        # Release everything if job is finished
+        self.cap.release()
+        self.outfile.release()
+        cv.destroyAllWindows()
 
-# def main():
-#     vid = VideoCapture()
-#     vid.capture_video()
-#     vid.free_video_resources()
+def main():
+    vid = VideoCapture()
+    vid.capture_video()
+    vid.free_video_resources()
 
-# if __name__ == '__main__':
-#     main()
-
-import cv2
-
-cap = cv2.VideoCapture(0)
-
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret==True:
-        frame = cv2.flip(frame,0)
-
-        # write the flipped frame
-        out.write(frame)
-
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
-
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
