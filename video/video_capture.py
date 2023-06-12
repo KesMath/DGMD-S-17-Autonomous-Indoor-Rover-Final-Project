605060c6a8f64ed3dbf4df5dcaffe66338a77f04
Kesler Mathieu <kmat5@icloud.com>
2023-06-12 17:56:58 -0400

    adding video capture script

---
+++ video/video_capture.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+
+class VideoCapture():
+    def __init__(self):
+        self.cap = None
+        self.outfile = None
+
+    def capture_video():
+        self.cap = cv.VideoCapture(0)
+
+        # Define the codec and create VideoWriter object
+        fourcc = cv.VideoWriter_fourcc(*'XVID')
+
+        self.outfile = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
+
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+            frame = cv.flip(frame, 0)
+            # write the flipped frame
+            self.outfile.write(frame)
+            cv.imshow('frame', frame)
+            if cv.waitKey(1) == ord('q'):
+                break
+
+    def free_video_resources():      
+        # Release everything if job is finished
+        self.cap.release()
+        self.outfile.release()
+        cv.destroyAllWindows()
+
+def main():
+    vid = VideoCapture()
+    vid.capture_video()
+    vid.free_video_resources()
+
+if __name__ == '__main__':
+    main()
