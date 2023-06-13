6c8bf6a542c005cd78a24947999efdf73d3c7351
Kesler Mathieu <kmat5@icloud.com>
2023-06-12 20:24:32 -0400

    trying to read camera device

--- video/video_capture.py
+++ video/video_capture.py
@@ -1,8 +1,9 @@
 import cv2 as cv
+# https://docs.opencv.org/4.x/d4/d15/group__videoio__flags__base.html#gga023786be1ee68a9105bf2e48c700294da0c5f2eb9d2d43fb3582583203160a419
 
 def capture_video():
     print("instantiating resources...")
-    cap = cv.VideoCapture(cv.CAP_V4L2 + 8)
+    cap = cv.VideoCapture("/dev/v4l/by-id/usb-GENERAL_GENERAL_WEBCAM-video-index0", cv.CAP_V4L2)
 
     # Define the codec and create VideoWriter object
     fourcc = cv.VideoWriter_fourcc(*'XVID')
