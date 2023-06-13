b7b6f9d5b6a5257f0b45c07c6c39baccaadde049
Kesler Mathieu <kmat5@icloud.com>
2023-06-12 20:03:05 -0400

    this change produces no error

--- video/video_capture.py
+++ video/video_capture.py
@@ -1,53 +1,7 @@
 import cv2 as cv
 
-# https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html
-# https://docs.opencv.org/4.x/de/db1/group__videoio__registry.html
-# https://github.com/opencv/opencv/issues/19527
-
-
-# class VideoCapture():
-#     def __init__(self):
-#         self.cap = None
-#         self.outfile = None
-
-#     def capture_video(self):
-#         self.cap = cv.VideoCapture(cv.CAP_V4L2) #cv2.CAP_V4L2
-
-#         # Define the codec and create VideoWriter object
-#         fourcc = cv.VideoWriter_fourcc(*'XVID')
-
-#         self.outfile = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
-
-#         while self.cap.isOpened():
-#             ret, frame = self.cap.read()
-#             if not ret:
-#                 print("Can't receive frame (stream end?). Exiting ...")
-#                 break
-#             frame = cv.flip(frame, 0)
-#             # write the flipped frame
-#             self.outfile.write(frame)
-#             cv.imshow('frame', frame)
-#             if cv.waitKey(1) == ord('q'):
-#                 break
-
-#     def free_video_resources(self):      
-#         # Release everything if job is finished
-#         self.cap.release()
-#         self.outfile.release()
-#         cv.destroyAllWindows()
-
-# def main():
-#     vid = VideoCapture()
-#     vid.capture_video()
-#     vid.free_video_resources()
-
-# if __name__ == '__main__':
-#     main()
-
-
-
 def capture_video():
-    cap = cv.VideoCapture(0) #cv.CAP_V4L2
+    cap = cv.VideoCapture(0)
 
     # Define the codec and create VideoWriter object
     fourcc = cv.VideoWriter_fourcc(*'XVID')
