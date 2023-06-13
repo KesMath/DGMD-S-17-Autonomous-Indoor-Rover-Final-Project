7e4b12ed399229763fc1a6d163bbcce7278c256c
Kesler Mathieu <kmat5@icloud.com>
2023-06-12 20:43:49 -0400

    small change

--- video/video_capture.py
+++ video/video_capture.py
@@ -1,9 +1,8 @@
 import cv2 as cv
-# https://docs.opencv.org/4.x/d4/d15/group__videoio__flags__base.html#gga023786be1ee68a9105bf2e48c700294da0c5f2eb9d2d43fb3582583203160a419
 
 def capture_video():
     print("instantiating resources...")
-    cap = cv.VideoCapture("/dev/v4l/by-id/usb-GENERAL_GENERAL_WEBCAM-video-index0")
+    cap = cv.VideoCapture("/dev/v4l/by-id/usb-GENERAL_GENERAL_WEBCAM-video-index0", cv.CAP_V4L2)
 
     if not cap.isOpened():
         print("Cannot open camera")
