693818e62159d21a4a9e22c5a2cceda409f01aa8
Kesler Mathieu <kmat5@icloud.com>
2023-06-15 14:44:51 -0400

    confirming code runs on laptop

--- video/video_capture.py
+++ video/video_capture.py
@@ -1,38 +1,39 @@
 import cv2 as cv
 
-def capture_video():
-    print("instantiating resources...")
-    cap = cv.VideoCapture("/dev/v4l/by-id/usb-GENERAL_GENERAL_WEBCAM-video-index1", cv.CAP_V4L2)
+class VideoCapture():
+    def __init__(self):
+        self.cap = None
+        self.outfile = None
 
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
+    def capture_video(self):
+        self.cap = cv.VideoCapture(0)
 
-    # Define the codec and create VideoWriter object
-    fourcc = cv.VideoWriter_fourcc(*'XVID')
+        # Define the codec and create VideoWriter object
+        fourcc = cv.VideoWriter_fourcc(*'XVID')
 
-    outfile = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
+        self.outfile = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
 
-    print("about to iterate through capture device...")
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        frame = cv.flip(frame, 0)
-        # write the flipped frame
-        outfile.write(frame)
-        cv.imshow('frame', frame)
-        if cv.waitKey(1) == ord('q'):
-            break
-    
-    print("releasing resources...")
-    cap.release()
-    outfile.release()
-    cv.destroyAllWindows()
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+            self.outfile.write(frame)
+            cv.imshow('frame', frame)
+            if cv.waitKey(1) == ord('q'):
+                self.free_video_resources()
+                break
+
+    def free_video_resources(self):      
+        # Release everything when job is finished
+        print("releasing resources...")
+        self.cap.release()
+        self.outfile.release()
+        cv.destroyAllWindows()
 
 def main():
-    capture_video()
+    vid = VideoCapture()
+    vid.capture_video()
 
 if __name__ == '__main__':
     main()
